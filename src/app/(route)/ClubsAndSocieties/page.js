801ed5be73eb4cf4340e5ce3647d483740efcb05
Vansh Mehta <mvansh705@gmail.com>
2024-08-04 23:05:27 +0530

    init

--- src/app/(route)/ClubsAndSocieties/page.js
+++ src/app/(route)/ClubsAndSocieties/page.js
@@ -1,12 +1,24 @@
-import ShiningText from '@/app/_components/ShiningText'
-import React from 'react'
+import ShiningText from "@/app/_components/ShiningText";
+import React from "react";
+import Cabinet from "./_components/Cabinet";
+import Council from "./_components/Council";
 
 function ClubsAndSocietiesList() {
   return (
-    <div className="flex justify-center">
-      <ShiningText  text="ClubsAndSocieties" />
+    <div>
+      <div className="flex justify-center">
+        <ShiningText text="ClubsAndSocieties" />
+      </div>
+
+      <div>
+        <Cabinet />
+      </div>
+
+      <div>
+        <Council />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ClubsAndSocietiesList
+export default ClubsAndSocietiesList;
