797de5f7af9ad39221e733336d4b71e44da01bfb
Vansh Mehta <mvansh705@gmail.com>
2024-08-05 15:16:46 +0530

    Changed the background

--- src/app/(route)/[slug]/page.js
+++ src/app/(route)/[slug]/page.js
@@ -7,16 +7,12 @@ function ClubsAndSocietiesList({ params }) {
   return (
     <div>
       <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-        <div className="fixed top-0 -z-10 h-full w-full">
-          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        </div>
         {/* this css is for mobile compatibility */}
 
         <div className="container mx-auto px-8">
           {/* this also same for mobile compatible */}
           <div className="flex justify-center">
             <ShiningText text="ClubsAndSocieties" />
-            {JSON.stringify(params, null, 2)}
           </div>
           {/* <div className="mt-4 ">
             <General />
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <section className="max-w-[640px] m-auto   px-9 md:px-0">
-          <Navbar/>
-       
+        <div className="fixed top-0 -z-10 h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        </div>
+        <section className="max-w-[640px] m-auto px-9 md:px-0">
+          <Navbar />
+
           {children}
         </section>
       </body>
