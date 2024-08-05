18915e8602dbf1409e921885c759f43ba7acad05
Mannan <Mannan@DESKTOP-OTIQOF5>
2024-08-05 15:32:27 +0530

    Mannan: Remove unused components and update ClubsAndSocieties page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,30 @@
+import ClubAndChapters from "./_components/ClubAndChapters";
 import ShiningText from "./_components/ShiningText";
+import React from "react";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center h-[100vh] bg-[#222]">
-        <ShiningText text="Kiva" />
+      <div>
+      <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+        {/* this css is for mobile compatibility */}
+
+        <div className="container mx-auto px-8">
+          {/* this also same for mobile compatible */}
+          <div className="flex justify-center">
+            <ShiningText text="ClubsAndSocieties" />
+          </div>
+          {/* <div className="mt-4 ">
+            <General />
+          </div> */}
+
+          <div>
+            <ClubAndChapters />
+          </div>
+        </div>
       </div>
+    </div>
+
     </>
   );
 }
